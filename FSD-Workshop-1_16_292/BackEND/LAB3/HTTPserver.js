43e4e6cbcc95c0ca958981a83df9b3c73a87e2fa
Arsalan Qamar <arsalanqamar2006@gmail.com>
2026-09-21 15:09:32 +0530

    remaining commit

--- FSD-Workshop-1_16_292/BackEND/LAB3/HTTPserver.js
+++ FSD-Workshop-1_16_292/BackEND/LAB3/HTTPserver.js
@@ -1,37 +1,59 @@
 import http from "http";
-
-const userdata = [{
-    id: 101,
-    name: "Abc",
-    email: "cm@abes.call.in"
-}];
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/msg" && method === "GET") {
-        res.setHeader("Content-Type", "text/plain");
-        res.statusCode = 200;
+const userData =[{
+    id : 101 ,
+    name : "Abc" ,
+    email : "cm@abes.call.in" 
+}]
+const server=http.createServer((req,res)=>{
+    //   res.statusCode=201;
+    //   res.setHeader("Content-type","text/plane");
+    //   res.end("Hello Serever.")
+    const url=req.url;
+    const method=req.method;
+    if(url=="/msg" && method == "GET"){
         res.end("This is welcome message from server");
     }
-    else if (url === "/sys" && method === "GET") {
-        res.setHeader("Content-Type", "text/plain");
-        res.statusCode = 200;
+    else if(url=="/sys" && method == "GET"){
         res.end("This is system information");
     }
-    else if (url === "/data" && method === "GET") {
-        res.setHeader("Content-Type", "application/json");
-        res.statusCode = 200;
-        res.end(JSON.stringify(userdata)); 
+    else if (url =="/data" && method =="GET") {
+        res.end(JSON.stringify(userData)) ;
+    } else if (url=="/create" && method=="POST") {
+        let body = "" ;
+        req.on("data",(chunk)=>{
+            body += chunk ;
+        })
+        req.on("end",()=> {
+          const newData =JSON.parse(body) ;
+          const newuserData = {
+            id : newData.id ,
+            name:newData.name,
+            email:newData.email
+          }  
+          userData.push(newuserData)
+          res.end("User created succesfully")
+        })
+    }else if (url=="/users" && method == "GET") {
+        res.end(JSON.stringify(userData)) ;
+    }else if (url.startsWith("/users/") && method =="GET") {
+        const id = url.split("/")[2] ;
+        console.log(id) ;
+        const user = userData.find((u) => u.id == id) ;
+        if (!user){
+            return res.end("User Not found ") ;
+        }
+        res.end(JSON.stringify(user));
+    } else if (url.startsWith("/delete/") && method =="DELETE") {
+       const id = url.split("/")[2] ;
+        console.log(id) ;
+        const user = userData.findIndex((u) => u.id == id) ;
+        if (user == -1){
+            return res.end("User Not found ") ;
+        }
+        userData.splice(user, 1)
+        res.end("Data deleted succesfully")
     }
-    else {
-        res.setHeader("Content-Type", "text/plain");
-        res.statusCode = 404;
-        res.end("404 Not Found");
-    }
-});
-
-server.listen(3000, () => {
-    console.log("Server is running on port number 3000");
-});
+})
+server.listen(4000,()=>{
+    console.log("Server is running on port number 4000");
+})
